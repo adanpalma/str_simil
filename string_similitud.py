236bb2c88f0063dfe15f551557c625abcec9e7c7
adan.palma <adanpalma@hotmail.com>
2021-10-07 20:05:50 -0500

    Agregue lectura del archivo csv  y hace las comparaciones

--- string_similitud.py
+++ string_similitud.py
@@ -1,41 +1,41 @@
 import textdistance as txtdist
+import csv
 
 from nltk.translate import bleu
 from nltk.translate.bleu_score import SmoothingFunction
 
 
 # nltk it is a translate package that can be used as a string comparison
-def compara_similutes(ptext1, ptext2):
-	smoothie = SmoothingFunction().method4
-	disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
-	disjaccard = txtdist.jaccard(ptext1, ptext2)
-	dissorensen = txtdist.sorensen(ptext1, ptext2)
-	dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
-	
-	print(f"\n {ptext1} - {ptext2} \n")
-	print("   Bleu          Jaccard          Sorensen       Sorensen_Dice")
-	print(
-			f"  {(disbleu * 100).__round__(2)}%           {(disjaccard * 100).__round__(2)}% "
-			f"           {(dissorensen * 100).__round__(2)}%        "
-			f" {(dissorensen_dice * 100).__round__(2)} "
-			f"               Promedio"
-			f" "
-			f"{(((disjaccard + dissorensen) / 2) * 100).__round__(2)}   %"
-			)
-	print("*" * 70)
+def compara_similutes(ptext1, ptext2) -> float:
+    smoothie = SmoothingFunction().method4
+    disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
+    disjaccard = txtdist.jaccard(ptext1, ptext2)
+    dissorensen = txtdist.sorensen(ptext1, ptext2)
+    dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
+
+    return ((((disbleu + disjaccard + dissorensen) / 3) * 100).__round__(2))
 
 
-text1: str = 'ADAN MANUEL PALMA DE LEON'
-text2: str = 'DE LEON MANUEL ADAN'
-text3: str = 'Farmacias Arrocha'
+registros = 1
 
-compara_similutes(text1.upper(), text2.upper())
-compara_similutes(text1.upper(), text3.upper())
-print("\n" * 5)
+empresa = list(csv.reader(open("inputs\\nombre_empresas.csv")))
+empresa1 = []
 
-text1: str = 'Proyecto las Acacias No1'
-text2: str = 'Las Acacias'
-text3: str = 'Farmacias Arrocha'
+try:
+    for nombre in empresa:
+        empresa1.append( nombre[0].split("-")[0])
+except:
+    pass
 
-compara_similutes(text1.upper(), text2.upper())
-compara_similutes(text1.upper(), text3.upper())
+empresa2 = empresa1.copy()
+
+for idx, nombre_empresa in enumerate(empresa1[1:]):
+
+    for idx1, nombre_empresa2 in enumerate(empresa2[1:]):
+        try:
+            avg = compara_similutes(nombre_empresa, nombre_empresa2)
+
+            if avg > 88.00 and avg < 100.00:
+                print(nombre_empresa, nombre_empresa2, avg)
+        except:
+            pass
