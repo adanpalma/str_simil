468eb22c8474f79a1df2aca8132130aeedbb9913
adan.palma <adanpalma@hotmail.com>
2021-10-09 20:16:20 -0500

    Agregue codigo para registar similitudes en un dict...

--- string_similitud.py
+++ string_similitud.py
@@ -1,43 +1,74 @@
-import textdistance as txtdist
-import csv
+from pathlib import Path
 
+import textdistance as txtdist
 from nltk.translate import bleu
 from nltk.translate.bleu_score import SmoothingFunction
 
+similitudes = {}
+detalle_empresa = {}
+coincidencias = []
+empresa_sin_codigo, codigo_empresa = [], []
+min_avg = 90.00
+max_avg = 99.9
+
+
+def registra_similitudes(
+		cod_empresa1: str, nombre_empresa1: str, cod_empresa2: str, nombre_empresa2: str,
+		avg: float
+		) -> dict:
+	if not (cod_empresa1 in similitudes):
+		similitudes[cod_empresa1] = {}
+		similitudes[cod_empresa1][nombre_empresa1] = []
+	
+	similitudes[cod_empresa1][nombre_empresa1].append(
+			{'codigo':cod_empresa2, 'nombre':nombre_empresa2, 'avg':avg}
+			)
+
 
 # nltk it is a translate package that can be used as a string comparison
-def compara_similutes(ptext1, ptext2) -> float:
-    smoothie = SmoothingFunction().method4
-    disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
-    disjaccard = txtdist.jaccard(ptext1, ptext2)
-    dissorensen = txtdist.sorensen(ptext1, ptext2)
-    dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
+def compara_similutes(ptext1: str, ptext2: str) -> float:
+	smoothie = SmoothingFunction().method4
+	disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
+	disjaccard = txtdist.jaccard(ptext1, ptext2)
+	dissorensen = txtdist.sorensen(ptext1, ptext2)
+	# dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
+	
+	return ((((disbleu + disjaccard + dissorensen) / 3) * 100).__round__(2))
 
-    return ((((disbleu + disjaccard + dissorensen) / 3) * 100).__round__(2))
 
+# TODO: TRATA DE USAR UN HASH O JSON PARA QUE MUESTRES EL CODIGO DE LAS EMPRESAS PARECIDAS
 
-registros = 1
+file_to_open = Path("inputs/nombre_empresas.csv")
+with open(file_to_open) as f:
+	empresa_y_codigo = f.readlines()
 
-#TODO: TRATA DE USAR UN HASH O JSON PARA QUE MUESTRES EL CODIGO DE LAS EMPRESAS PARECIDAS
-
-empresa = list(csv.reader(open("inputs\\nombre_empresas.csv")))
-empresa1 = []
-
+# Separo descripcion y codigo ya que viene en la descripcion y los guardo por separado
 try:
-    for nombre in empresa:
-        empresa1.append( nombre[0].split("-")[0])
+	for nombre in empresa_y_codigo:
+		empresa_sin_codigo.append(nombre.split("-")[0].strip('\n'))
+		codigo_empresa.append(nombre.split("-")[1].strip('\n'))
 except:
-    pass
+	pass
 
-empresa2 = empresa1.copy()
+empresa_comparar = empresa_sin_codigo.copy()
 
-for idx, nombre_empresa in enumerate(empresa1[1:]):
+for idx, nombre_empresa1 in enumerate(empresa_sin_codigo[1:]):
 
-    for idx1, nombre_empresa2 in enumerate(empresa2[1:]):
-        try:
-            avg = compara_similutes(nombre_empresa, nombre_empresa2)
+	for idx1, nombre_empresa2 in enumerate(empresa_comparar[1:]):
+		print(f"Comparando {idx} -> {idx1}", end='\r')
+		
+		
+		try:
+			avg = compara_similutes(nombre_empresa1, nombre_empresa2)
+			if avg > min_avg and avg < max_avg:
+				registra_similitudes(
+						codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1],
+						nombre_empresa2,
+						avg
+						)
+		
+		except:
+			pass
 
-            if avg > 88.00 and avg < 100.00:
-                print(nombre_empresa, nombre_empresa2, avg)
-        except:
-            pass
+
+print("Terminando", similitudes)
