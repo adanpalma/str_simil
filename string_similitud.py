bbcaaa9d5bc8c5a798d181b0384c3e4bdeec7dcd
adan.palma <adanpalma@hotmail.com>
2021-10-10 21:03:21 -0500

    Instale el paquete strsimpy.jaro_winkler

--- string_similitud.py
+++ string_similitud.py
@@ -1,12 +1,13 @@
-from nltk.translate import bleu
-from nltk.translate.bleu_score import SmoothingFunction
 from pathlib import Path
-import textdistance as txtdist
 import json
-import jellyfish
-import textdistance as txtdist
 
-similitudes  = {}
+from strsimpy.jaro_winkler import JaroWinkler
+from pathlib import Path
+import json
+
+from strsimpy.jaro_winkler import JaroWinkler
+
+similitudes = {}
 empresa_sin_codigo, codigo_empresa = [], []
 min_avg = 89.99
 max_avg = 100
@@ -23,17 +24,19 @@ def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: 
 
 # nltk it is a translate package that can be used as a string comparison
 def compara_similutes(ptext1: str, ptext2: str) -> float:
-	#smoothie = SmoothingFunction().method4
-	#disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
-	dissorensen = txtdist.sorensen(ptext1, ptext2)
-	disjaccard = txtdist.jaccard(ptext1,ptext2)
+	# smoothie = SmoothingFunction().method4
+	# disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
+	# dissorensen = txtdist.sorensen(ptext1, ptext2)
+	# disjaccard = txtdist.jaccard(ptext1,ptext2)
 	# dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
-	jellyscore = jellyfish.jaro_distance(ptext1,ptext2)
-	return(jellyscore * 100)
+	# jellyscore = jellyfish.jaro_distance(ptext1,ptext2)
+	jarowinkler = JaroWinkler()
+	strsimil = jarowinkler.similarity(ptext1, ptext2)
 	
-	#return (((( disjaccard + dissorensen) / 2) * 100).__round__(2))
-	
-	
+	return (strsimil*100)
+
+
+
 
 file_to_open = Path("inputs/nombre_empresas.csv")
 with open(file_to_open) as f:
@@ -63,8 +66,8 @@ for idx, nombre_empresa1 in enumerate(empresa_sin_codigo):
 			avg = 0
 		
 		if avg > min_avg and avg <= max_avg:
-			registra_similitudes(codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1], nombre_empresa2, avg)
-			#print(nombre_empresa1," - ", nombre_empresa2, avg)
+			registra_similitudes(codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1], nombre_empresa2,
+			                     avg)  # print(nombre_empresa1," - ", nombre_empresa2, avg)
 
 pathtofile = Path('output/similitudes.json')
 with open(pathtofile, 'w') as outputfile:
