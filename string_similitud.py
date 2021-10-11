d43c6e4350037c604c1f24681bf49071050250ef
adan.palma <adanpalma@hotmail.com>
2021-10-10 20:28:56 -0500

    Cambie algoritmos de busqueda y comparacion ahora esty con jellyfish
    He incoporado la rutina jellyfish, udando el jaro_distance y agregue manejo de json

--- string_similitud.py
+++ string_similitud.py
@@ -1,74 +1,71 @@
-from pathlib import Path
-
-import textdistance as txtdist
 from nltk.translate import bleu
 from nltk.translate.bleu_score import SmoothingFunction
+from pathlib import Path
+import textdistance as txtdist
+import json
+import jellyfish
+import textdistance as txtdist
 
-similitudes = {}
-detalle_empresa = {}
-coincidencias = []
+similitudes  = {}
 empresa_sin_codigo, codigo_empresa = [], []
-min_avg = 90.00
-max_avg = 99.9
+min_avg = 89.99
+max_avg = 100
 
 
-def registra_similitudes(
-		cod_empresa1: str, nombre_empresa1: str, cod_empresa2: str, nombre_empresa2: str,
-		avg: float
-		) -> dict:
+def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: str, nombre_empresa2: str,
+		avg: float) -> dict:
 	if not (cod_empresa1 in similitudes):
 		similitudes[cod_empresa1] = {}
 		similitudes[cod_empresa1][nombre_empresa1] = []
 	
-	similitudes[cod_empresa1][nombre_empresa1].append(
-			{'codigo':cod_empresa2, 'nombre':nombre_empresa2, 'avg':avg}
-			)
+	similitudes[cod_empresa1][nombre_empresa1].append({'codigo': cod_empresa2, 'nombre': nombre_empresa2, 'avg': avg})
 
 
 # nltk it is a translate package that can be used as a string comparison
 def compara_similutes(ptext1: str, ptext2: str) -> float:
-	smoothie = SmoothingFunction().method4
-	disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
-	disjaccard = txtdist.jaccard(ptext1, ptext2)
+	#smoothie = SmoothingFunction().method4
+	#disbleu = bleu([ptext1], ptext2, smoothing_function=smoothie)
 	dissorensen = txtdist.sorensen(ptext1, ptext2)
+	disjaccard = txtdist.jaccard(ptext1,ptext2)
 	# dissorensen_dice = txtdist.sorensen_dice(ptext1, ptext2)
+	jellyscore = jellyfish.jaro_distance(ptext1,ptext2)
+	return(jellyscore * 100)
 	
-	return ((((disbleu + disjaccard + dissorensen) / 3) * 100).__round__(2))
-
-
-# TODO: TRATA DE USAR UN HASH O JSON PARA QUE MUESTRES EL CODIGO DE LAS EMPRESAS PARECIDAS
+	#return (((( disjaccard + dissorensen) / 2) * 100).__round__(2))
+	
+	
 
 file_to_open = Path("inputs/nombre_empresas.csv")
 with open(file_to_open) as f:
 	empresa_y_codigo = f.readlines()
 
 # Separo descripcion y codigo ya que viene en la descripcion y los guardo por separado
+
+
+#empresa_y_codigo = [empresa for empresa in empresa_y_codigo if 'PLAYA BONITA' in empresa]
+
 try:
 	for nombre in empresa_y_codigo:
-		empresa_sin_codigo.append(nombre.split("-")[0].strip('\n'))
-		codigo_empresa.append(nombre.split("-")[1].strip('\n'))
+		empresa_sin_codigo.append(nombre.split("*")[0].strip('\n'))
+		codigo_empresa.append(nombre.split("*")[1].strip('\n'))
 except:
 	pass
 
 empresa_comparar = empresa_sin_codigo.copy()
 
-for idx, nombre_empresa1 in enumerate(empresa_sin_codigo[1:]):
-
-	for idx1, nombre_empresa2 in enumerate(empresa_comparar[1:]):
-		print(f"Comparando {idx} -> {idx1}", end='\r')
+for idx, nombre_empresa1 in enumerate(empresa_sin_codigo):
+	
+	for idx1, nombre_empresa2 in enumerate(empresa_comparar):
 		
-		
-		try:
+		if codigo_empresa[idx] != codigo_empresa[idx1]:
 			avg = compara_similutes(nombre_empresa1, nombre_empresa2)
-			if avg > min_avg and avg < max_avg:
-				registra_similitudes(
-						codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1],
-						nombre_empresa2,
-						avg
-						)
+		else:
+			avg = 0
 		
-		except:
-			pass
+		if avg > min_avg and avg <= max_avg:
+			registra_similitudes(codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1], nombre_empresa2, avg)
+			#print(nombre_empresa1," - ", nombre_empresa2, avg)
 
-
-print("Terminando", similitudes)
+pathtofile = Path('output/similitudes.json')
+with open(pathtofile, 'w') as outputfile:
+	json.dump(similitudes, outputfile, indent=3)
