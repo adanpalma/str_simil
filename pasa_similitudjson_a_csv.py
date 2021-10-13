0f3283cb77c485383a29ce92d115a5e6fe3f87c8
adan.palma <adanpalma@hotmail.com>
2021-10-12 21:28:59 -0500

    Agregue para manejar el Salvador

--- pasa_similitudjson_a_csv.py
+++ pasa_similitudjson_a_csv.py
@@ -3,12 +3,12 @@ import json
 from pathlib import Path
 
 def pasa_similitudes_a_csv(pais):
-	in_path_n_file = Path('output/similitudes_'+pais+'.json')
-	out_path_n_file = Path('output/similitudes_'+pais+'.csv')
+	in_path_n_file = Path('output/similitudes_' + pais + '.json')
+	out_path_n_file = Path('output/similitudes_' + pais + '.csv')
 
 	out_file =  open(out_path_n_file,"w")
 
-	simil_writer = csv.writer(out_file, delimiter =',',quotechar='"', quoting= csv.QUOTE_MINIMAL)
+	simil_writer = csv.writer(out_file, delimiter =',')
 	simil_writer.writerow(['Cod Empresa1','Nombre Empresa1','Cod Emrpesa2','Nombre Empresa2', 'Avg1','Avg2'])
 
 
@@ -21,3 +21,4 @@ def pasa_similitudes_a_csv(pais):
 				simil_writer.writerow([cod_empresa,similitudes[cod_empresa]['Nombre Empresa'],registros['codigo'],registros['nombre'],registros['avgjrowinkler'].__round__(2),registros['avgjellymatchratingcodex'].__round__(2)])
 
 	out_file.close()
+
--- string_similitud.py
+++ string_similitud.py
@@ -4,6 +4,8 @@ from tkinter.filedialog import askopenfilename
 import jellyfish
 from strsimpy.jaro_winkler import JaroWinkler
 import pasa_similitudjson_a_csv as pasacsv
+from tkinter.filedialog import askopenfilename
+from tkinter import *
 
 similitudes = {}
 empresa_sin_codigo, codigo_empresa = [], []
@@ -11,7 +13,6 @@ min_avg = 94.99
 max_avg = 100
 
 pais = input(" Indique PAN  o SAL ?")
-
 
 def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: str, nombre_empresa2: str, avg: float,
                          avg1) -> None:
@@ -21,7 +22,6 @@ def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: 
     similitudes[cod_empresa1]['simil'].append(
         {'codigo': cod_empresa2, 'nombre': nombre_empresa2, 'avgjrowinkler': avg, 'avgjellymatchratingcodex':
             avg1})
-
 
 def compara_similutes(ptext1: str, ptext2: str):
     jarowinkler = JaroWinkler()
@@ -37,25 +37,28 @@ def compara_similutes(ptext1: str, ptext2: str):
 
     return strsimil * 100, strsimil2 * 100
 
-
-
-
 # This is where we lauch the file manager bar.
-from tkinter.filedialog import askopenfilename
+
+tkroot = Tk()
 name = askopenfilename(initialdir="~/inputs/",
                        filetypes=(("Text File", "*.*"), ("All Files", "*.*")),
                        title="Choose a file."
                        )
-
-
+tkroot.destroy()
+tkroot.quit()
 
 file_to_open = Path(name)
+if not file_to_open.is_file():
+    print('\n Debe indicar un nombre de archivo..')
+    exit(0)
+
 with open(file_to_open) as f:
     empresa_y_codigo = f.readlines()
 
-# empresa_y_codigo = [empresa for empresa in empresa_y_codigo if 'PLAYA BONITA' in empresa]
 
+# empresa_y_codigo = [empresa for empresa in empresa_y_codigo if 'PLAYA BONITA' in empresa]
 # Separo descripcion y codigo ya que viene en la descripcion y los guardo por separado
+
 try:
     for nombre in empresa_y_codigo:
         empresa_sin_codigo.append(nombre.split("*")[0].strip('\n'))
@@ -66,19 +69,18 @@ except:
 empresa_comparar = empresa_sin_codigo.copy()
 
 for idx, nombre_empresa1 in enumerate(empresa_sin_codigo):
-
     for idx1, nombre_empresa2 in enumerate(empresa_comparar):
-
+    
         if codigo_empresa[idx] != codigo_empresa[idx1]:
             avg, avg1 = compara_similutes(nombre_empresa1, nombre_empresa2)
         else:
             avg, avg1 = 0, 0
-
         if (avg > min_avg and avg <= max_avg):
             registra_similitudes(codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1], nombre_empresa2, avg,
                                  avg1)  # print(nombre_empresa1," - ", nombre_empresa2, avg)
 
 pathtofile = Path('output/similitudes_' + pais +'.json')
+
 with open(pathtofile, 'w') as outputfile:
     json.dump(similitudes, outputfile, indent=3, ensure_ascii=False)
 
