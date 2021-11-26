176c2e4be13945b2ad2f92181835f1ba0f3e9fe4
adan.palma <adanpalma@hotmail.com>
2021-11-26 18:38:12 -0500

    Se agrega control para decidir cuantos algoritmos aplicar
    Se agrego un parametro para indicar si se desea usar uno o dos algoritmos de comparacion y por tanto para ser considerado como similitud ambos algoritmos deben  superar el umbral de minimo y maximo.
    Adicional se puso algo de comentario al inicio como para ayudar al lector a conocer como deb preparar el imput a procesar

--- string_similitud.py
+++ string_similitud.py
@@ -6,12 +6,27 @@ from strsimpy.jaro_winkler import JaroWinkler
 import pasa_similitudjson_a_csv as pasacsv
 from tkinter.filedialog import askopenfilename
 from tkinter import *
+import time
+import sys
+
+# Este script espera ustede le pase un archivo csv con una soloa columna
+#sin titulo y dentro de cada fila debe colocar la descripcion de lo que queire comparar
+# seguido de un * y el codigo interno que lleva en el sistema
+#ejemplo:   Proyecto1*1233
+#           Proyecto ZZZ*123
+#Con este formato el script funciona y sabe que lo que esta antes de los * es la desciprion a comparar
+#y lo que esta despues es el codigo que tiene en el sistema appx
+#el resultado es un .json y un .csv con cada una de las similitudes que superan el umbral de similitud
+#el resultado queda en el directorio output.
+
+
 
 
 similitudes = {}
 empresa_sin_codigo, codigo_empresa = [], []
 min_avg = 91.00
 max_avg = 100
+numero_algoritmos = 2
 
 
 def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: str,  nombre_empresa2: str,
@@ -24,17 +39,17 @@ def registra_similitudes(cod_empresa1: str, nombre_empresa1: str, cod_empresa2: 
     similitudes[cod_empresa1]['simil'].append(
         {'codigo': cod_empresa2, 'nombre': nombre_empresa2, 'avgjrowinkler': avg, 'avgjellymatchratingcodex':avg1})
 
-def compara_similutes(ptext1: str, ptext2: str):
+def compara_similutes(ptext1: str, ptext2: str, algoritmos=1):
     ptext1 = ptext1.strip()
     ptext2 = ptext2.strip()
     jarowinkler = JaroWinkler()
     strsimil, strsimil2 = 0, 0
     strsimil = jarowinkler.similarity(ptext1.split(' '), ptext2.split(' '))
 
-    metphon1 = jellyfish.metaphone(ptext1)
-    metphon2 = jellyfish.metaphone(ptext2)
-    strsimil2 = jarowinkler.similarity(metphon1.split(' '), metphon2.split(' '))
-
+    if algoritmos >1:
+        metphon1 = jellyfish.metaphone(ptext1)
+        metphon2 = jellyfish.metaphone(ptext2)
+        strsimil2 = jarowinkler.similarity(metphon1.split(' '), metphon2.split(' '))
 
     return strsimil * 100, strsimil2 * 100
 
@@ -60,10 +75,6 @@ if not file_to_open.is_file():
 with open(file_to_open) as f:
     empresa_y_codigo = f.readlines()
 
-
-# empresa_y_codigo = [empresa for empresa in empresa_y_codigo if 'PLAYA BONITA' in empresa]
-# Separo descripcion y codigo ya que viene en la descripcion y los guardo por separado
-
 try:
     for nombre in empresa_y_codigo:
         empresa_sin_codigo.append(nombre.split("*")[0].strip('\n'))
@@ -72,16 +83,24 @@ except:
     pass
 
 for idx, nombre_empresa1 in enumerate(empresa_sin_codigo):
+
     for idx1, nombre_empresa2 in enumerate(empresa_sin_codigo):
+        # print(idx1,end="\r")
+        # sys.stdout.flush()
+
 
         avg, avg1 = 0, 0
         # Esta comparacion es para controlar  cuando estoy en la misma empresa o se leyo una nueva empresa*
         if codigo_empresa[idx] != codigo_empresa[idx1]:
-            avg, avg1 = compara_similutes(nombre_empresa1, nombre_empresa2)
+            avg, avg1 = compara_similutes(nombre_empresa1, nombre_empresa2, numero_algoritmos)
 
 
-        if (avg > min_avg and avg <= max_avg) and (avg1 > min_avg and avg1 <= max_avg):
+        if ((numero_algoritmos == 1) and (avg > min_avg and avg <= max_avg)) \
+                or  ((numero_algoritmos == 2) and
+                     ((avg > min_avg and avg <= max_avg) and (avg1 > min_avg and avg1 <= max_avg))):
             registra_similitudes(codigo_empresa[idx], nombre_empresa1, codigo_empresa[idx1], nombre_empresa2, avg,avg1)
+
+
 
 pathtofile = Path('output/similitudes_' + tema  + pais +'.json')
 
